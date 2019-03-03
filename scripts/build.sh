271ff72cd75d3c0d7b6fe489f029f8d84dcb25da
Regene Vilvestre <regene_vilvestre@ligph.com>
2019-03-03 23:48:08 +0800

    sqldump

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 WP_MIX="wp-mix"
-MYSQL_CONTAINER="mysql-cont"
-WP_CONTAINER="wp-cont"
+MYSQL_CONTAINER="wpmix_mysql_1"
+WP_CONTAINER="wpmix_wp_1"
 
 # BUILD DOCKER IMAGE
-docker build -t $WP_MIX ./docker/
+docker build -t $WP_MIX .
 
 # BUILD CONTAINER
-# docker run --name $MYSQL_CONTAINER -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
-# docker run --name $WP_CONTAINER --link $MYSQL_CONTAINER:mysql -p 8000:80 -d WP_MIX
+docker run --name $MYSQL_CONTAINER -e $MYSQL_ROOT_PASSWORD=root -d mysql:5.7
+docker run --name $WP_CONTAINER --link $MYSQL_CONTAINER:mysql -p 8000:80 -d $WP_MIX
